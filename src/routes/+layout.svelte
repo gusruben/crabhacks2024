12b3bcdb900bdb5a5c2354c9c92cc21b959346d9
Archonic <gabrielc2904@gmail.com>
2024-12-01 13:59:06 -0500

    Finished gradient on navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '@fontsource/lexend/400.css';
+    import '@fontsource/lexend/700.css';
+  </script>
+  
+  <slot />
+  
+  <style>
+    :global(body) {
+      font-family: 'Lexend', sans-serif;
+    }
+  </style>
+  
